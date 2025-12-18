7c5ef7b891d9f82617eeef0f671c48f60d482cdf
ELFATEH001 <aimanenedjima@gmail.com>
2025-12-18 12:12:36 +0000

    Done reading Lab3 description

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt update
+cd ~/humble_ws/src/VNAV_23/vnav_ws/src/Lab3/tesse-interface
+pip install -r requirements.txt  # Dependencies
+pip install .
+
+sudo apt install ros-humble-tf-transformations
+pip install numpy==1.26.4
+pip install -U transforms3d
+pip install rospy2
+sudo apt install ros-humble-ackermann-msgs
+sudo apt install ros-humble-rviz-imu-plugin
+
+cd ~/humble_ws/src/VNAV_23/vnav_ws/src && git clone https://github.com/ethz-asl/mav_comm.git -b ros2
